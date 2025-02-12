ddd1e13fa1945808cb27accdb7f8469e30b29f23
LeongLRF <1915272283@qq.com>
2025-02-13 00:54:24 +0800

    fix

--- packages/core/src/components/Shop/PluginUploader/index.tsx
+++ packages/core/src/components/Shop/PluginUploader/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger, Form, FormControl, FormField, FormItem, FormLabel, Input, Step, Stepper, useForm, zodResolver } from "@repo/ui";
+import { Button, Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger, FileUploader, Form, FormControl, FormField, FormItem, FormLabel, Input, Step, Stepper, Textarea, useForm, zodResolver } from "@repo/ui";
 import React, { PropsWithChildren } from "react";
 import { z } from "@repo/ui";
 
@@ -17,6 +17,8 @@ export const PluginUploader: React.FC<PropsWithChildren> = ({ children }) => {
     const formSchema = z.object({
         name: z.string().min(2).max(50),
         key: z.string().min(2).max(50),
+        version: z.string(),
+        tags: z.string(),
         description: z.string().min(2).max(50),
     })
 
@@ -46,40 +48,100 @@ export const PluginUploader: React.FC<PropsWithChildren> = ({ children }) => {
     const render = () => {
         switch (currentStep) {
             case 1: return <Form {...form}>
-                <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Name</FormLabel>
-                            <FormControl>
-                                <Input {...field} />
-                            </FormControl>
-                        </FormItem>
-                    )}
-                />
+                <form className=" space-y-1">
+                    <div className=" grid grid-cols-2 gap-2">
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="key"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Key</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="version"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Version</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="tags"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Tags</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <div>
+                        <FormField
+                            control={form.control}
+                            name="description"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Description</FormLabel>
+                                    <FormControl>
+                                        <Textarea {...field} />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                </form>
             </Form>
-            default: return <div>123123</div>
+            case 4: return <div>
+                <FileUploader />
+            </div>
+            default: return <div>
+                <FileUploader />
+            </div>
         }
     }
 
     return <Dialog>
         <DialogTrigger asChild>{children}</DialogTrigger>
-        <DialogContent className="w-[800px] max-w-none">
+        <DialogContent className="w-[600px] max-w-none h-auto ">
             <DialogHeader>
-                <DialogTitle>上传插件</DialogTitle>
+                <DialogTitle>发布插件</DialogTitle>
                 <DialogDescription />
             </DialogHeader>
-            <Stepper
-                steps={steps}
-                currentStep={currentStep}
-                onStepClick={handleStepClick}
-                className="h-full"
-            />
-            {render()}
-            <div className="text-center space-x-1">
-                <Button onClick={handlePrev}>上一步</Button>
-                <Button onClick={handleNext}>下一步</Button>
+            <div className="min-h-[400px] space-y-2">
+                <Stepper
+                    steps={steps}
+                    currentStep={currentStep}
+                    onStepClick={handleStepClick}
+                />
+                {render()}
+                <div className="text-center space-x-1">
+                    <Button onClick={handlePrev}>上一步</Button>
+                    <Button onClick={handleNext}>下一步</Button>
+                </div>
             </div>
         </DialogContent>
     </Dialog>
