b5a687b75f1ff009e8dd4c98fac0abd438bb1a4d
albus <980159737@qq.com>
2025-02-14 09:05:05 +0800

    管理插件

--- packages/core/src/components/Shop/PluginManager/index.tsx
+++ packages/core/src/components/Shop/PluginManager/index.tsx
@@ -5,11 +5,15 @@ import React, { PropsWithChildren } from "react";
 export const PluginManager: React.FC<PropsWithChildren> = ({ children }) => {
     return <Dialog>
         <DialogTrigger asChild>{children}</DialogTrigger>
-        <DialogContent className=" max-w-none w-[80%] h-[80%]">
+        <DialogContent className=" max-w-none w-[80%]">
             <DialogHeader>
-                <DialogTitle></DialogTitle>
-                <DialogDescription></DialogDescription>
+                <DialogTitle>管理你的插件</DialogTitle>
             </DialogHeader>
+            <div className="grid grid-cols-[200px_1fr] h-[calc(100vh*0.8)] w-full">
+                <div className="h-full w-full border-r">
+                    32123
+                </div>
+            </div>
         </DialogContent>
     </Dialog>
 }
