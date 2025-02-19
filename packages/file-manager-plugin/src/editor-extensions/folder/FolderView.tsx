917931d6bdcd6be77668537e61f2d853b5db7626
albus <980159737@qq.com>
2025-02-19 17:23:44 +0800

    refactor

--- packages/file-manager-plugin/src/editor-extensions/folder/FolderView.tsx
+++ packages/file-manager-plugin/src/editor-extensions/folder/FolderView.tsx
@@ -1,11 +1,17 @@
 import React from "react"
 import { NodeViewProps, NodeViewWrapper } from "@repo/editor"
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator, Button, Input } from "@repo/ui"
+import { PlusCircle, UploadIcon } from "@repo/icon"
 
 export const FolderView: React.FC<NodeViewProps> = (props) => {
 
     const { node: { attrs } } = props
 
     return <NodeViewWrapper as="div" className=" border rounded-sm w-full h-[300px]">
-        <div></div>
+        <div className="w-full border-b flex items-center gap-1 p-1">
+            <Button variant={"ghost"}> <PlusCircle /> 新建文件夹</Button>
+            <Button variant={"ghost"}><UploadIcon /> 上传文件</Button>
+            <Input placeholder="搜索文件" className=" w-[200px] h-7" />
+        </div>
     </NodeViewWrapper>
 }
