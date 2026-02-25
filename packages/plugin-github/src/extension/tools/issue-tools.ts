18efbed32a79d45ba653c8cd2353b5a12e2fb7da
albus <980159737@qq.com>
2026-02-25 15:46:51 +0800

    fix

--- packages/plugin-github/src/extension/tools/issue-tools.ts
+++ packages/plugin-github/src/extension/tools/issue-tools.ts
@@ -3,7 +3,6 @@ import { z } from '@kn/ui'
 import { PluginConfigStore } from '@kn/core'
 import { getIssue, listIssues, createIssue, updateIssue, getIssueComments, addIssueComment } from '../../services/github-issue-service'
 import type { GitHubPluginConfig } from '../../types/config'
-import { DEFAULT_GITHUB_CONFIG } from '../../types/config'
 import { GITHUB_PLUGIN_KEY } from '../../hooks/use-github-config'
 
 async function getToken(): Promise<string> {
